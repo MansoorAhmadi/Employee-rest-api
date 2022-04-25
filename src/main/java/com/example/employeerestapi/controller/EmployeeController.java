c9c075a1de6957feb02fe922bb2b4440e9087722
itsmefreak <ahmadi.mansoor@outlook.com>
2022-04-25 09:42:13 +0200

    PostMapping createEmployee

--- src/main/java/com/example/employeerestapi/controller/EmployeeController.java
+++ src/main/java/com/example/employeerestapi/controller/EmployeeController.java
@@ -3,10 +3,8 @@ package com.example.employeerestapi.controller;
 import com.example.employeerestapi.model.Employee;
 import com.example.employeerestapi.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +21,11 @@ public class EmployeeController {
     public List<Employee> getAllEmployees(){
         return repository.findAll();
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee){
+        return repository.save(employee);
+    }
+    
 }
